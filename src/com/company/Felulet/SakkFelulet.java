44fde208f691b581a03fab752c628ae581f5e43c
Diak <ads@asd.asd>
2020-01-28 18:49:57 +0100

    mennekmarhazahelp

--- src/com/company/Felulet/SakkFelulet.java
+++ src/com/company/Felulet/SakkFelulet.java
@@ -56,6 +56,14 @@ public class SakkFelulet extends JFrame {
         }
 
         for (int i = 0; i < 8; i++) {
+            JLabel hivatkozas123 = new JLabel();
+            hivatkozas123.setText(String.format("%d.", (8-i)));
+            hivatkozas123.setHorizontalAlignment((SwingConstants.CENTER));
+            hivatkozas123.setVerticalAlignment((SwingConstants.CENTER));
+            this.pnlJatekTabla.add(hivatkozas123);
+
+
+
             for (int j = 0; j < 8; j++) {
                 JButton btn = new JButton();
                 Mezo m = new Mezo(i,j, tabla.getErtek(i, j));
