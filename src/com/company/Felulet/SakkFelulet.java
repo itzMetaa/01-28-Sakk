bae0d174224eb3b157d49f0c2c8d61d735d72b49
Diak <ads@asd.asd>
2020-01-28 18:20:27 +0100

    yey már látszik is

---
+++ src/com/company/Felulet/SakkFelulet.java
@@ -0,0 +1,61 @@
+package com.company.Felulet;
+
+import com.company.Logika.SakkTabla;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class SakkFelulet extends JFrame {
+
+    private SakkTabla tabla;
+
+    private Container foAblak;
+    private JPanel pnlJatekTabla;
+
+    public SakkFelulet() {
+        this.tabla = new SakkTabla();
+        this.initComponents();
+
+        this.sakkTablaMegjelenit();
+    }
+
+    private void initComponents(){
+
+        this.setTitle("Sakk 1.0");
+
+        int width = (int)Toolkit.getDefaultToolkit().getScreenSize().getWidth();
+        int height = (int)Toolkit.getDefaultToolkit().getScreenSize().getHeight();
+        this.setSize(width, height);
+
+        this.setAlwaysOnTop(true);
+        this.setLocationRelativeTo(null);
+
+        this.foAblak = this.getContentPane();
+        this.foAblak.setLayout(new BorderLayout(10,10));
+
+        this.pnlJatekTabla = new JPanel();
+        this.pnlJatekTabla.setLayout(new GridLayout(9,9));
+
+        this.foAblak.add(BorderLayout.CENTER, this.pnlJatekTabla);
+
+
+
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
+
+    private void sakkTablaMegjelenit(){
+        this.pnlJatekTabla.removeAll();
+
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                JButton btn = new JButton();
+                btn.setText(this.tabla.getErtek(i,j) + "");
+                this.pnlJatekTabla.add(btn);
+            }
+        }
+
+        this.revalidate();
+        this.repaint();
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,16 +1,21 @@
 package com.company;
 
+import com.company.Felulet.SakkFelulet;
 import com.company.Logika.SakkTabla;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        SakkFelulet f = new SakkFelulet();
+
+        /*
         SakkTabla t = new SakkTabla();
         System.out.println(t);
         t.lep(6,4,4,4);
         System.out.println(t);
         t.lep(6,3,4,2);
         System.out.println(t);
+         */
     }
 }
